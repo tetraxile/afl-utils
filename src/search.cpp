79098f99c7239bbe47b4bbdca41f1c718c6561c2
tetraxile <tetraxile@proton.me>
2025-04-22 20:11:34 +0100

    make smo search results look nicer

--- src/search.cpp
+++ src/search.cpp
@@ -1,7 +1,12 @@
 #include <cassert>
 #include <cstdio>
 #include <filesystem>
+#include <map>
+#include <optional>
 #include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 #include <afl/byml/reader.h>
@@ -9,6 +14,7 @@
 #include <afl/sarc.h>
 #include <afl/types.h>
 #include <afl/util.h>
+#include <afl/vector.h>
 #include <afl/yaz0.h>
 
 namespace fs = std::filesystem;
@@ -19,17 +25,57 @@ enum class Game {
 };
 
 struct Query {
-    std::string name;
-    bool isRecurse = false;
+    const std::string name;
+    const bool isRecurse = false;
 };
 
 struct Result {
-    std::string stageName;
-    u32 scenarioIdx;
-    std::string itemList;
-    std::string objId;
-    Vector3f trans;
+    const std::string stageName;
+    mutable std::array<bool, 15> scenarioFlag;
+    const u32 scenarioIdx;
+    const std::string itemList;
+
+    const std::string unitConfigName;
+    const std::string modelName;
+    const std::string paramConfigName;
+    const std::string objId;
+    const Vector3f trans;
+    const Vector3f rotate;
+    const Vector3f scale;
+
+    bool operator==(const Result& other) const;
 };
+
+template <>
+struct std::hash<Result> {
+    std::size_t operator()(const Result& res) const noexcept {
+        size_t out = 0;
+        util::hashCombine(out, res.stageName);
+        util::hashCombine(out, res.itemList);
+        util::hashCombine(out, res.unitConfigName);
+        util::hashCombine(out, res.modelName);
+        util::hashCombine(out, res.paramConfigName);
+        util::hashCombine(out, res.objId);
+        util::hashCombine(out, res.trans);
+        util::hashCombine(out, res.rotate);
+        util::hashCombine(out, res.scale);
+        return out;
+    }
+};
+
+bool Result::operator==(const Result& other) const {
+    return std::hash<Result>{}(*this) == std::hash<Result>{}(other);
+    // return util::isEqual(stageName, other.stageName) &&
+    //        util::isEqual(itemList, other.itemList) &&
+    //        util::isEqual(unitConfigName, other.unitConfigName) &&
+    //        util::isEqual(paramConfigName, other.paramConfigName) &&
+    //        util::isEqual(modelName, other.modelName) &&
+    //        util::isEqual(objId, other.objId) &&
+    //        trans.isEqual(other.trans) &&
+    //        scale.isEqual(other.scale) &&
+    //        rotate.isEqual(other.rotate);
+}
+
 
 bool endsWith(const std::string& fullString, const std::string& ending) {
     if (fullString.length() >= ending.length())
@@ -86,20 +132,20 @@ result_t SearchEngine::loadStage(std::vector<u8>& bymlContents, const fs::path& 
     return 0;
 }
 
-result_t readVec3f(Vector3f* out, const byml::Reader& reader) {
+result_t readVec3f(Vector3f* out, const byml::Reader& reader, const std::string& name) {
     result_t r;
 
-    byml::Reader translate;
-    r = reader.getContainerByKey(&translate, "Translate");
+    byml::Reader vec;
+    r = reader.getContainerByKey(&vec, name);
     if (r) return r;
 
-    r = translate.getF32ByKey(&out->x, "X");
+    r = vec.getF32ByKey(&out->x, "X");
     if (r) return r;
 
-    r = translate.getF32ByKey(&out->y, "Y");
+    r = vec.getF32ByKey(&out->y, "Y");
     if (r) return r;
 
-    r = translate.getF32ByKey(&out->z, "Z");
+    r = vec.getF32ByKey(&out->z, "Z");
     if (r) return r;
 
     return 0;
@@ -127,28 +173,38 @@ result_t SearchEngine::searchScenario(const byml::Reader& scenario) {
             r = item.getStringByKey(&unitConfigName, "UnitConfigName");
             if (r) return r;
 
-            std::string objId;
-            r = item.getStringByKey(&objId, "Id");
-            if (r) return r;
-
             byml::Reader unitConfig;
             r = item.getContainerByKey(&unitConfig, "UnitConfig");
             if (r) return r;
 
-            std::string parameterConfigName;
-            r = unitConfig.getStringByKey(&parameterConfigName, "ParameterConfigName");
+            std::string paramConfigName;
+            r = unitConfig.getStringByKey(&paramConfigName, "ParameterConfigName");
             if (r) return r;
 
             std::string modelName;
             bool hasModelName = item.tryGetStringByKey(&modelName, "ModelName");
 
             if (util::isEqual(unitConfigName, mQuery.name) ||
-                util::isEqual(parameterConfigName, mQuery.name) ||
+                util::isEqual(paramConfigName, mQuery.name) ||
                 (hasModelName && util::isEqual(modelName, mQuery.name))) {
                 Vector3f trans;
-                readVec3f(&trans, item);
+                readVec3f(&trans, item, "Translate");
+                Vector3f rotate;
+                readVec3f(&rotate, item, "Rotate");
+                Vector3f scale;
+                readVec3f(&scale, item, "Scale");
 
-                Result result = { mCurStageName, mCurScenarioIdx, mCurItemList, objId, trans };
+                std::string objId;
+                r = item.getStringByKey(&objId, "Id");
+                if (r) return r;
+
+                std::string optModelName = hasModelName ? modelName : "";
+                std::array<bool, 15> scenarioFlag = { false };
+
+                if (mGame == Game::SMO)
+                    scenarioFlag[mCurScenarioIdx] = true;
+                
+                Result result = { mCurStageName, scenarioFlag, mCurScenarioIdx, mCurItemList, unitConfigName, optModelName, paramConfigName, objId, trans, rotate, scale };
                 mResults.push_back(result);
             }
         }
@@ -220,18 +276,44 @@ result_t SearchEngine::searchAllStages(const fs::path& romfsPath) {
 
     printf("found %zu matches\n", mResults.size());
 
-
-
     return 0;
 }
 
 void SearchEngine::saveResults(const fs::path& outPath) const {
     FILE* f = fopen(outPath.c_str(), "w");
 
-    for (const Result& result : mResults) {
-        if (mGame == Game::SMO)
-            fprintf(f, "%s %d %s %s\n", result.stageName.c_str(), result.scenarioIdx, result.itemList.c_str(), result.objId.c_str());
-        else if (mGame == Game::SM3DW)
+    if (mGame == Game::SMO) {
+        std::unordered_set<Result> collapsedResults;
+        for (const auto& result : mResults) {
+            if (collapsedResults.contains(result))
+                collapsedResults.find(result)->scenarioFlag[result.scenarioIdx] = true;
+            else
+                collapsedResults.insert(result);
+        }
+
+        std::map<std::string, std::vector<Result>> stages;
+        for (const auto& result : collapsedResults) {
+            if (stages.contains(result.stageName))
+                stages[result.stageName].push_back(result);
+            else
+                stages[result.stageName] = std::vector<Result> { result };
+        }
+
+        for (const auto& [stageName, results] : stages) {
+            fprintf(f, "%s:\n", stageName.c_str());
+            for (const auto& result : results) {
+                fprintf(f, "\tUnitConfigName: %s\n", result.unitConfigName.c_str());
+                fprintf(f, "\tTranslate: (%.3f, %.3f, %.3f)\n", result.trans.x, result.trans.y, result.trans.z);
+                fprintf(f, "\tId: %s\n", result.objId.c_str());
+                fprintf(f, "\tscenarios: ");
+                for (u32 i = 0; i < result.scenarioFlag.size(); i++)
+                    if (result.scenarioFlag[i])
+                        fprintf(f, "%d, ", i+1);
+                fprintf(f, "\n\n");
+            }
+        }
+    } else if (mGame == Game::SM3DW) {
+        for (const Result& result : mResults)
             fprintf(f, "%s\t%s\t%s\n", result.stageName.c_str(), result.itemList.c_str(), result.objId.c_str());
     }
 
@@ -248,7 +330,6 @@ s32 main(s32 argc, char** argv) {
     std::string romfsPath = argv[2];
     std::string objectName = argv[3];
     std::string outPath = argc < 5 ? "results.txt" : argv[4];
-    // std::string outPath = "./result.txt";
 
     Game game;
     if (util::isEqual(gameName, "smo"))
